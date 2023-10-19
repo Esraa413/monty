a91ca9ba99745b955dfec9b5cb641bea5830309f
Esraa413 <esraaabdelwahab614@gmail.com>
2023-10-19 14:22:48 -0700

    stacks

---
+++ main.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "monty.h"
+
+stack_t *head = NULL;
+
+/**
+ * main - the entry to point
+ * @argc: arguments
+ * @argv: list of arguments conts
+ *
+ * Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	open_file(argv[1]);
+	free_nodes();
+
+	return (0);
+}
+
+/**
+ * create_node -the function to Creates a node
+ * @n: Number to go inside the node
+ *
+ * Return: Always 0
+ */
+
+stack_t *create_node(int n)
+{
+	stack_t *node = malloc(sizeof(stack_t));
+
+	if (node == NULL)
+	{
+		err(4);
+	}
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+
+	return (node);
+}
+/**
+ * free_nodes - the function to frees node to the stack
+ */
+void free_nodes(void)
+{
+	stack_t *tmp;
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+/**
+ * add_to_queue - thr function to  adds a node to queue.
+ * @new_node: Pointer to new node.
+ * @ln: line number
+ */
+
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+
+	tmp->next = *new_node;
+	(*new_node)->prev = tmp;
+
+}
---
+++ tools_fun.c
@@ -0,0 +1,169 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "monty.h"
+
+/**
+ * open_file - the function to open a file
+ * @file_name: file name_path
+ *
+ * Return: Always (0).
+ *
+ */
+void open_file(char *file_name)
+{
+	FILE *fd = fopen(file_name, "read");
+
+	if (file_name == NULL || fd == NULL)
+	{
+		err(2, file_name);
+	}
+	read_file(fd);
+	fclose(fd);
+}
+/**
+ * read_file - the function to  read a file
+ * @fd: file pointer to descriptor
+ *
+ * Return: Always (0).
+ */
+
+void read_file(FILE *fd)
+{
+	int line = 0;
+	int format = 0;
+	char *buffer = NULL;
+	size_t len_fd = 0;
+
+	line = 1;
+	while (getline(&buffer, &len_fd, fd) != -1)
+	{
+		format = parse_line(buffer, line, format);
+		line++;
+	}
+	free(buffer);
+}
+
+/**
+ * parse_line - Each line is separated into distinct symbols to identify
+ * the function to be called
+ * @buffer: the line from file
+ * @line_number: the line  to number
+ * @format: format string. If 0 The nodes will be entered as a stack
+ * if 1 The node will be entered into the queue
+ *
+ * Return: Always (0)
+ */
+
+int parse_line(char *buffer, int line_number, int format)
+{
+	char *opcode, *value;
+	const char *delim = "\n ";
+
+	if (buffer == NULL)
+	{
+		err(4);
+	}
+	opcode = strtok(buffer, delim);
+	if (opcode == NULL)
+	{
+		return (format);
+	}
+	value = strtok(NULL, delim);
+	if (strcmp(opcode, "stack") == 0)
+		return (0);
+	if (strcmp(opcode, "queue") == 0)
+		return (1);
+	find_func(opcode, value, line_number, format);
+
+	return (format);
+}
+
+/**
+ * find_func - the function to find the appropriate function for the opcode
+ * @opcode: it is opcode
+ * @value: argument to the opcode
+ * @ln: line number
+ * @format: format string. If 0 The nodes will be entered as a stack
+ * if 1 The node will be entered into the queue
+ * Return: Always (0)
+ */
+
+void find_func(char *opcode, char *value, int ln, int format)
+{
+	int x;
+	int flag_cd;
+
+	instruction_t func_list[] = {
+		{"push", add_to_stack},
+		{"pall", print_stack},
+		{"pint", print_top},
+		{"pop", pop_top},
+		{"nop", nop},
+		{"swap", swap_nodes},
+		{"add", add_nodes},
+		{"sub", sub_nodes},
+		{"div", div_nodes},
+		{"mul", mul_nodes},
+		{"mod", mod_nodes},
+		{"pchar", print_char},
+		{"pstr", print_str},
+		{"rotl", rotl},
+		{"rotr", rotr},
+		{NULL, NULL}
+	};
+	if (opcode[0] == '#')
+		return;
+
+	for (flag _cd = 1, x = 0; func_list[x].opcode != NULL; x++)
+	{
+		if (strcmp(opcode, func_list[x].opcode) == 0)
+		{
+			call_fun(func_list[x].f, opcode, value, ln, format);
+			flag_cd = 0;
+		}
+	}
+	if (flag == 1)
+		err(3, ln, opcode);
+}
+
+/**
+ * call_fun - the function to Calls the required function
+ * @func: The pointer to function  is about to be called
+ * @op: string  It  is represents opcode
+ * @val: string it is representing a numeric value
+ * @ln: line numeber
+ * @format: format string. If 0 The nodes will be entered as a stack
+ * if 1 The node will be entered into the queue
+ */
+void call_fun(op_func func, char *op, char *val, int ln, int format)
+{
+	stack_t *node;
+	int flag_cd;
+	int x;
+
+	flag_cd = 1;
+	if (strcmp(op, "push") == 0)
+	{
+		if (val != NULL && val[0] == '-')
+		{
+			val = val + 1;
+			flag_cd = -1;
+		}
+		if (val == NULL)
+			err(5, ln);
+		x = 0;
+		while (val[x] != '\0')
+		{
+			if (isdigit(val[x]) == 0)
+				err(5, ln);
+			x++;
+		}
+		node = create_node(atoi(val) * flag_cd);
+		if (format == 0)
+			func(&node, ln);
+		if (format == 1)
+			add_to_queue(&node, ln);
+	}
+	else
+		func(&head, ln);
+}
