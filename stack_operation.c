86e4026c5ed7a650a363d1a5a6bbe864bd4d431a
Mariam <eng.mariamnabil93@gmail.com>
2023-10-19 03:34:14 -0700

    monty

--- stack_operation.c
+++ stack_operation.c
@@ -1,26 +1,37 @@
 #include "monty.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
-/***/
+/**
+ * mul_nodes - function to add the top two elements
+ * @stack: a pointer pointing to top node
+ * @line_number: Interger the line number
+*/
 
 void mul_nodes(stack_t **stack, unsigned int line_number)
 {
-	int sum;
+	int s;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 		more_err(8, line_number, "mul");
 
 	(*stack) = (*stack)->next;
-	sum = (*stack)->n * (*stack)->prev->n;
-	(*stack)->n = sum;
+	s = (*stack)->n * (*stack)->prev->n;
+	(*stack)->n = s;
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
 
-/***/
+/**
+ * mod_nodes - function to add the top two elements
+ * @stack: a pointer pointing to top node
+ * @line_number: Interger the line number
+*/
 
 void mod_nodes(stack_t **stack, unsigned int line_number)
 {
-	int sum;
+	int s;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 
@@ -30,8 +41,8 @@ void mod_nodes(stack_t **stack, unsigned int line_number)
 	if ((*stack)->n == 0)
 		more_err(9, line_number);
 	(*stack) = (*stack)->next;
-	sum = (*stack)->n % (*stack)->prev->n;
-	(*stack)->n = sum;
+	s = (*stack)->n % (*stack)->prev->n;
+	(*stack)->n = s;
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
