bf62452024142c8b53017dbc6fabcd82b006abe9
Mariam <eng.mariamnabil93@gmail.com>
2023-10-19 03:22:43 -0700

    monty

--- stack_function2.c
+++ stack_function2.c
@@ -1,6 +1,13 @@
 #include "monty.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
-/***/
+/**
+ * nop - function to do nothing.
+ * @stack: pointing to top node of the stack.
+ * @line_number: Interger input line number
+*/
 
 void nop(stack_t **stack, unsigned int line_number)
 {
@@ -8,7 +15,11 @@ void nop(stack_t **stack, unsigned int line_number)
 	(void)line_number;
 }
 
-/***/
+/**
+ * swap_nodes - function to swap the two element
+ * @stack: pointing to top node of the stack.
+ * @line_number: Interger line number
+*/
 
 void swap_nodes(stack_t **stack, unsigned int line_number)
 {
@@ -26,27 +37,35 @@ void swap_nodes(stack_t **stack, unsigned int line_number)
 	*stack = tmp;
 }
 
-/***/
+/**
+ * add_nodes - function to add the top two elements
+ * @stack: pointing to top node of the stack.
+ * @line_number: Interger line number
+*/
 
 void add_nodes(stack_t **stack, unsigned int line_number)
 {
-	int sum;
+	int s;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 		more_err(8, line_number, "add");
 
 	(*stack) = (*stack)->next;
-	sum = (*stack)->n + (*stack)->prev->n;
-	(*stack)->n = sum;
+	s = (*stack)->n + (*stack)->prev->n;
+	(*stack)->n = s;
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
 
-/***/
+/**
+ * sub_nodes - function to add the top two elements
+ * @stack: pointer pointing to top node
+ * @line_number: Interger the line number
+*/
 
 void sub_nodes(stack_t **stack, unsigned int line_number)
 {
-	int sum;
+	int s;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 
@@ -54,17 +73,21 @@ void sub_nodes(stack_t **stack, unsigned int line_number)
 
 
 	(*stack) = (*stack)->next;
-	sum = (*stack)->n - (*stack)->prev->n;
-	(*stack)->n = sum;
+	s = (*stack)->n - (*stack)->prev->n;
+	(*stack)->n = s;
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
 
-/***/
+/**
+ * div_nodes - function to add the top two elements
+ * @stack: pointing to top node of the stack.
+ * @line_number: Interger line number
+*/
 
 void div_nodes(stack_t **stack, unsigned int line_number)
 {
-	int sum;
+	int s;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 		more_err(8, line_number, "div");
@@ -72,8 +95,8 @@ void div_nodes(stack_t **stack, unsigned int line_number)
 	if ((*stack)->n == 0)
 		more_err(9, line_number);
 	(*stack) = (*stack)->next;
-	sum = (*stack)->n / (*stack)->prev->n;
-	(*stack)->n = sum;
+	s = (*stack)->n / (*stack)->prev->n;
+	(*stack)->n = s;
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
