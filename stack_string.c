2f56bd0e6fb1869ff351507b7719d5406f3adbf1
Mariam <eng.mariamnabil93@gmail.com>
2023-10-19 03:47:34 -0700

    monty

--- stack_string.c
+++ stack_string.c
@@ -1,6 +1,10 @@
 #include "monty.h"
 
-/***/
+/**
+ * print_char - function to print the Ascii value
+ * @stack: a pointer pointing to top node
+ * @line_number: Interger the line number
+*/
 
 void print_char(stack_t **stack, unsigned int line_number)
 {
@@ -15,22 +19,40 @@ void print_char(stack_t **stack, unsigned int line_number)
 	printf("%c\n", ascii);
 }
 
-/***/
+/**
+ * print_str - function to print a string.
+ * @stack: a pointer pointing to top node
+ * @line_number: Interger the line number
+*/
 
-void print_char(stack_t **stack, unsigned int line_number)
+void print_str(stack_t **stack, __attribute__((unused))unsigned int ln)
 {
 	int ascii;
+	stack_t *tmp;
 
 	if (stack == NULL || *stack == NULL)
-		string_err(11, line_number);
+	{
+		printf("\n");
+		return;
+	}
 
-	ascii = (*stack)->n;
-	if (ascii < 0 || ascii > 127)
-		string_err(10, line_number);
-	printf("%c\n", ascii);
+	tmp = *stack;
+	while (tmp != NULL)
+	{
+		ascii = tmp->n;
+		if (ascii <= 0 || ascii > 127)
+			break;
+		printf("%c", ascii);
+		tmp = tmp->next;
+	}
+	printf("\n");
 }
 
-/***/
+/**
+ * rotl - function to rotate the first node of the stack
+ * @stack: a pointer pointing to top node
+ * @ln: Interger the line number
+*/
 
 void rotl(stack_t **stack, __attribute__((unused))unsigned int ln)
 {
@@ -50,7 +72,11 @@ void rotl(stack_t **stack, __attribute__((unused))unsigned int ln)
 	(*stack)->prev = NULL;
 }
 
-/***/
+/**
+ * rotr - function to rotate the last node of the stack
+ * @stack: a pointer pointing to top node
+ * @ln: Interger the line number
+*/
 
 void rotr(stack_t **stack, __attribute__((unused))unsigned int ln)
 {
